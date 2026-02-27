a138da69355a2c5a3daeebe030be997d2a89a4f0
Edgar Ivan Patricio <edgaripatricio@tecmilenio.mx>
2026-02-27 15:45:23 -0600

    Remove unused API import from NuevoProductoPage component

--- src/app/(protected)/productos/nuevo/page.js
+++ src/app/(protected)/productos/nuevo/page.js
@@ -1,7 +1,6 @@
 'use client';
 
 import { useState } from 'react';
-import { API } from '@/config';
 import { apiFetch } from '@/lib/api';
 import StatusBox from '@/components/StatusBox';
 import { useRouter } from 'next/navigation';
